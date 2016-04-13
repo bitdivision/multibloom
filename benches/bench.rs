a9c3a1b511c45e1b9305d575ef7d8bc8972caa00
Richard Fortescue-Webb <rfwebb@gmail.com>
2016-04-13 22:18:30 +0100

    Benchmarks for xxHash. Significantly faster for larger input

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,25 +3,26 @@
 extern crate multibloom;
 extern crate test;
 extern crate rand;
+extern crate twox_hash;
 
 use test::{Bencher, black_box};
 use multibloom::BloomFilter;
 use rand::Rng;
+use twox_hash::RandomXxHashBuilder;
 
 #[bench]
-fn bench_add_1(b: &mut Bencher) {
+fn bench_add_1_sip(b: &mut Bencher) {
     let mut bloom = BloomFilter::new(1000, 10);
     let bytes = rand::thread_rng().gen_iter::<u8>().take(1).collect::<Vec<u8>>();
-    let testing = 100;
     b.iter(|| {
-        bloom.add(&testing);
+        bloom.add(&bytes);
         black_box(&bloom);
     });
 }
 
 
 #[bench]
-fn bench_add_64(b: &mut Bencher) {
+fn bench_add_64_sip(b: &mut Bencher) {
     let mut bloom = BloomFilter::new(1000, 10);
     let bytes = rand::thread_rng().gen_iter::<u8>().take(64).collect::<Vec<u8>>();
 
@@ -31,9 +32,8 @@ fn bench_add_64(b: &mut Bencher) {
     });
 }
 
-
 #[bench]
-fn bench_add_1k(b: &mut Bencher) {
+fn bench_add_1k_sip(b: &mut Bencher) {
     let mut bloom = BloomFilter::new(1000, 10);
     let bytes = rand::thread_rng().gen_iter::<u8>().take(1024).collect::<Vec<u8>>();
     b.iter(|| {
@@ -41,3 +41,35 @@ fn bench_add_1k(b: &mut Bencher) {
         black_box(&bloom);
     });
 }
+
+#[bench]
+fn bench_add_1_xx_hash(b: &mut Bencher) {
+    let mut bloom = BloomFilter::new_with_hasher(RandomXxHashBuilder::default(), 1000, 10);
+    let bytes = rand::thread_rng().gen_iter::<u8>().take(1).collect::<Vec<u8>>();
+    b.iter(|| {
+        bloom.add(&bytes);
+        black_box(&bloom);
+    });
+}
+
+#[bench]
+fn bench_add_64_xx_hash(b: &mut Bencher) {
+    let mut bloom = BloomFilter::new_with_hasher(RandomXxHashBuilder::default(), 1000, 10);
+    let bytes = rand::thread_rng().gen_iter::<u8>().take(64).collect::<Vec<u8>>();
+
+    b.iter(|| {
+        bloom.add(&bytes);
+        black_box(&bloom);
+    });
+}
+
+#[bench]
+fn bench_add_1k_xx_hash(b: &mut Bencher) {
+    let mut bloom = BloomFilter::new_with_hasher(RandomXxHashBuilder::default(), 1000, 10);
+    let bytes = rand::thread_rng().gen_iter::<u8>().take(1024).collect::<Vec<u8>>();
+    b.iter(|| {
+        bloom.add(&bytes);
+        black_box(&bloom);
+    });
+}
+
